08c9b3a228b3483717d8511bcbbeee38887ffc36
Jake Ferrigan <jakeferrigan@Mac.lan>
2025-09-16 01:06:09 -0500

    revise landing page & fix deploy scripts

--- rollout.sh
+++ rollout.sh
@@ -1,7 +1,12 @@
+export PROD_SPOTIFY_REDIRECT_URI=https://mindgobblin.com/callback
+
 ssh jake@165.22.233.151 '
   docker rm -f jake-srv || true
   docker run -d --restart unless-stopped --name jake-srv \
     -e ASPNETCORE_URLS=http://0.0.0.0:8080 \
+    -e SPOTIFY_CLIENT_ID=$SPOTIFY_CLIENT_ID \
+    -e SPOTIFY_CLIENT_SECRET=$SPOTIFY_CLIENT_SECRET  \
+    -e SPOTIFY_REDIRECT_URI=$PROD_SPOTIFY_REDIRECT_URI \
     -p 127.0.0.1:8080:8080 \
     jferrigan/jake-server:latest
 '
